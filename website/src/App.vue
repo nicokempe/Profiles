0b4c44ee7099454bc8d6c5c78195a910514080d0
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-05-19 16:26:21 +0200

    Remove the AboutView
    * Removed the router-link for the default about page
    * Removed the AboutView file
    * Added the Home, SignIn, SignUp and Profile views to the navigation

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/signin">Sign in</router-link> |
+    <router-link to="/signup">Sign up</router-link> |
+    <router-link to="/profile">Profile</router-link>
   </nav>
   <router-view/>
 </template>
