825d2163ad75dd82dd15999e080c1e22caf30d89
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-05-23 16:21:19 +0200

    Add the pages to the Router
    * Imprint
    * Privacy Policy
    * Forget Password
    * Settings
    * Sign In
    * Sign Up

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -2,20 +2,64 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
+  // Default Views
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
   {
-    path: '/profile',
-    name: 'profile',
+    path: '/imprint',
+    name: 'imprint',
+    component: function () {
+      return import('../views/ImprintView.vue')
+    },
+  },
+  {
+    path: '/privacy-policy',
+    name: 'privacy-policy',
+    component: function () {
+      return import('../views/PrivacyPolicyView.vue')
+    },
+  },
+  // Profile Views
+  {
+    path: '/forget-password',
+    name: 'forget-password',
     // route level code-splitting
-    // this generates a separate chunk (profile.[hash].js) for this route
+    // this generates a separate chunk (forget-password.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "profile" */ '../views/profile/ProfileView.vue')
-    }
+      return import(/* webpackChunkName: "forget-password" */ '../views/profile/ForgetPassword.vue')
+    },
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: function () {
+      return import('../views/profile/ProfileView.vue')
+    },
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: function () {
+      return import('../views/profile/SettingsView.vue')
+    },
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: function () {
+      return import('../views/profile/SignInView.vue')
+    },
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: function () {
+      return import('../views/profile/SignUpView.vue')
+    },
   }
 ]
 
